4172acdc2375ef2e12d0fe396066e23634f2f95f
David Morgan <david_a_morgan@icloud.com>
2021-02-26 14:03:18 -0500

    Shorthand function wrappers for variables

--- btt/variable.go
+++ btt/variable.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"strconv"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -52,6 +53,34 @@ func (b *BTT) GetVariable(ctx context.Context, name string, persistent, number b
 	return buf.String(), nil
 }
 
+// GetStringVariable is shorthand for GetVariable(ctx, name, false, false)
+func (b *BTT) GetStringVariable(ctx context.Context, name string) (string, error) {
+	return b.GetVariable(ctx, name, false, false)
+}
+
+// GetNumberVariable is shorthand for GetVariable(ctx, name, false, true)
+func (b *BTT) GetNumberVariable(ctx context.Context, name string) (float64, error) {
+	s, err := b.GetVariable(ctx, name, false, true)
+	if err != nil {
+		return -1, err
+	}
+	return strconv.ParseFloat(s, 64)
+}
+
+// GetPersistentStringVariable is shorthand for GetVariable(ctx, name, true, false)
+func (b *BTT) GetPersistentStringVariable(ctx context.Context, name string) (string, error) {
+	return b.GetVariable(ctx, name, true, false)
+}
+
+// GetPersistentNumberVariable is shorthand for GetVariable(ctx, name, true, true)
+func (b *BTT) GetPersistentNumberVariable(ctx context.Context, name string) (float64, error) {
+	s, err := b.GetVariable(ctx, name, true, true)
+	if err != nil {
+		return -1, err
+	}
+	return strconv.ParseFloat(s, 64)
+}
+
 // SetVariable sets the specified variable to the specified value in BTT
 func (b *BTT) SetVariable(ctx context.Context, name, value string, persistent, number bool) error {
 	log.WithFields(log.Fields{
@@ -89,4 +118,24 @@ func (b *BTT) SetVariable(ctx context.Context, name, value string, persistent, n
 	defer resp.Body.Close()
 	io.Copy(ioutil.Discard, resp.Body)
 	return nil
+}
+
+// SetStringVariable is shorthand for SetVariable(ctx, name, value, false, false)
+func (b *BTT) SetStringVariable(ctx context.Context, name, value string) error {
+	return b.SetVariable(ctx, name, value, false, false)
+}
+
+// SetNumberVariable is shorthand for SetVariable(ctx, name, value, false, true)
+func (b *BTT) SetNumberVariable(ctx context.Context, name string, value float64) error {
+	return b.SetVariable(ctx, name, fmt.Sprintf("%f", value), false, true)
+}
+
+// SetPersistentStringVariable is shorthand for SetVariable(ctx, name, value, true, false)
+func (b *BTT) SetPersistentStringVariable(ctx context.Context, name, value string) error {
+	return b.SetVariable(ctx, name, value, true, false)
+}
+
+// SetPersistentNumberVariable is shorthand for SetVariable(ctx, name, value, true, true)
+func (b *BTT) SetPersistentNumberVariable(ctx context.Context, name string, value float64) error {
+	return b.SetVariable(ctx, name, fmt.Sprintf("%f", value), true, true)
 }
