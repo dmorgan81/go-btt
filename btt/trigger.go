f3e97a7fab44b237e87f48092d5ae80673417d78
David Morgan <david_a_morgan@icloud.com>
2021-03-12 14:49:37 -0500

    Trigger add command

--- btt/trigger.go
+++ btt/trigger.go
@@ -1,6 +1,7 @@
 package btt
 
 import (
+	"bytes"
 	"context"
 	"io"
 	"io/ioutil"
@@ -8,25 +9,25 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// GetTrigger returns the JSON representation of the trigger with the specified UUID
+// GetTrigger returns the JSON representation of the trigger with the specified UUID.
 func (b *BTT) GetTrigger(ctx context.Context, uuid string, w io.Writer) error {
 	log.WithField("uuid", uuid).Debug("GetTrigger")
 	return b.execute(ctx, "get_trigger", map[string]string{"uuid": uuid}, w)
 }
 
-// ExecuteTrigger executes assigned actions for the trigger with the specified UUID
+// ExecuteTrigger executes assigned actions for the trigger with the specified UUID.
 func (b *BTT) ExecuteTrigger(ctx context.Context, uuid string) error {
 	log.WithField("uuid", uuid).Debug("ExecuteTrigger")
 	return b.execute(ctx, "execute_assigned_actions_for_trigger", map[string]string{"uuid": uuid}, ioutil.Discard)
 }
 
-// DeleteTrigger deletes the trigger with the specified UUID
+// DeleteTrigger deletes the trigger with the specified UUID.
 func (b *BTT) DeleteTrigger(ctx context.Context, uuid string) error {
 	log.WithField("uuid", uuid).Debug("DeleteTrigger")
 	return b.execute(ctx, "delete_trigger", map[string]string{"uuid": uuid}, ioutil.Discard)
 }
 
-// Trigger the named trigger with the specified name
+// Trigger the named trigger with the specified name.
 func (b *BTT) Trigger(ctx context.Context, name string, async bool) error {
 	log.WithField("name", name).Debug("Trigger")
 	action := "trigger_named"
@@ -36,6 +37,7 @@ func (b *BTT) Trigger(ctx context.Context, name string, async bool) error {
 	return b.execute(ctx, action, map[string]string{"trigger_name": name}, ioutil.Discard)
 }
 
+// UpdateTrigger updates the trigger with the specified UUID with JSON content from the specified reader.
 func (b *BTT) UpdateTrigger(ctx context.Context, uuid string, r io.Reader) error {
 	log.WithField("uuid", uuid).Debug("UpdateTrigger")
 	d, err := ioutil.ReadAll(r)
@@ -43,4 +45,20 @@ func (b *BTT) UpdateTrigger(ctx context.Context, uuid string, r io.Reader) error
 		return err
 	}
 	return b.execute(ctx, "update_trigger", map[string]string{"uuid": uuid, "json": string(d)}, ioutil.Discard)
+}
+
+// AddTrigger adds a trigger with JSON content from the specified reader.
+// Returns the UUID of the new trigger.
+func (b *BTT) AddTrigger(ctx context.Context, r io.Reader) (string, error) {
+	log.Debug("AddTrigger")
+	d, err := ioutil.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+
+	buf := &bytes.Buffer{}
+	if err := b.execute(ctx, "add_new_trigger", map[string]string{"json": string(d)}, buf); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
---
+++ cmd/triggerAdd.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/dmorgan81/go-btt/btt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var triggerAddCmd = &cobra.Command{
+	Use:     "add",
+	Aliases: []string{"new", "create"},
+	Args:    cobra.NoArgs,
+	Short:   "Add a new trigger with JSON from stdin",
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithTimeout(context.Background(), viper.GetDuration("timeout"))
+		defer cancel()
+
+		b := btt.New(viper.GetString("addr")).WithSecret(viper.GetString("secret"))
+		uuid, err := b.AddTrigger(ctx, os.Stdin)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(uuid)
+		}
+	},
+}
+
+func init() {
+	triggerCmd.AddCommand(triggerAddCmd)
+}
